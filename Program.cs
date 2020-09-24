6ec50e4031e13f40c952d70dba70bb4fb008aa01
Visgarg <71806030+Visgarg@users.noreply.github.com>
2020-09-24 18:22:05 +0530

    Update Program.cs
    creating conflict

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace git_practice_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello World!");
             Console.WriteLine("Hello Vishal!");
         }
     }
